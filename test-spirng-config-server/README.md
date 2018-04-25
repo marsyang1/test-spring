a32e38f7b6ee5fcc081b6d65d4b86972d1f63c85
yangyuchi <marsyang1@gmail.com>
2018-04-26 00:21:00 +0800

    add config server test code

---
+++ test-spirng-config-server/README.md
@@ -0,0 +1,3 @@
+# test spring cloud config server
+
+* reference : https://github.com/spring-cloud-samples/configserver
--- test-spirng-config-server/src/main/java/com/mars/testspirngconfigserver/App.java
+++ test-spirng-config-server/src/main/java/com/mars/testspirngconfigserver/App.java
@@ -2,11 +2,18 @@ package com.mars.testspirngconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+//@Configuration
+//@EnableAutoConfiguration
+//@EnableDiscoveryClient
+@EnableConfigServer
 @SpringBootApplication
 public class App {
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
+
+
 }
